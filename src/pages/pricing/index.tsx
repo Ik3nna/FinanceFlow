9dfc2140a4235c22980f845d0764e665b9e4d3aa
ik3nna <iamiykendukwu@gmail.com>
2023-08-05 18:18:37 +0100

    feat: Initial setup of the pricing page

---
+++ src/pages/pricing/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "./index.module.css"
+import Cards from '../../components/cards';
+import Hstack from '../../layouts/hstack';
+
+// assets
+
+
+const Pricing: React.FC = () => {
+  return (
+    <section className={styles.container}>
+
+    </section>
+  )
+}
+
+export default Pricing;
