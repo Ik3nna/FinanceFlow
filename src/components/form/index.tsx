9ca50a71a2f6dc531093b02d7193d731049240d4
ik3nna <iamiykendukwu@gmail.com>
2023-08-11 11:01:49 +0100

    feat: Form component

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -3,7 +3,43 @@ import styles from "./index.module.css";
 
 const Form: React.FC = () => {
   return (
-    <div>Form</div>
+    <form className={styles.component}>
+      <section className={styles.subComponents}>
+        <div>
+          <label htmlFor='name'>Name</label>
+
+          <input type='text' placeholder='Full Name' />
+        </div>
+
+        <div>
+          <label htmlFor='email'>Email</label>
+
+          <input type='email' placeholder='email@example.com' />
+        </div>
+      </section>
+
+      <section className={styles.subComponents}>
+        <div>
+          <label htmlFor='company'>Company</label>
+
+          <input type='text' placeholder='Company Name' />
+        </div>
+
+        <div>
+          <label htmlFor='subject'>Subject</label>
+
+          <input type='text' placeholder='How can we help?' />
+        </div>
+      </section>
+
+      <section className={styles.subComponents}>
+        <div>
+          <label htmlFor='message'>Message</label>
+
+          <input type="text" placeholder='Your Message' />
+        </div>
+      </section>
+    </form>
   )
 }
 
